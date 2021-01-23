b834e2b5488501704b23c608e4b80ac68dfd6657
mduumar112233 <mdumarfaruk187326@gmail.com>
2021-01-22 20:50:17 -0800

    finish assignmet

--- assignment.js
+++ assignment.js
@@ -1,81 +1,81 @@
-// ////// FIRST PROBLEM 
+////// FIRST PROBLEM 
 
-// function kilometerToMeter(meter){
-//     if (meter != 0 && meter > 0){
-//         var result = meter * 1000;
-//     }
-//     else{
-//         console.log("You must be input valid number for check this function!!!!!!!!")
-//     }
-//     return result;
-// }
+function kilometerToMeter(meter){
+    if (meter != 0 && meter > 0){
+        var result = meter * 1000;
+    }
+    else{
+        console.log("You must be input valid number for check this function!!!!!!!!")
+    }
+    return result;
+}
 
-// var userInput = kilometerToMeter(1);
-// console.log(userInput)
+var userInput = kilometerToMeter(3);
+console.log(userInput)
 
-// ////// SECOND PROBLEM 
+////// SECOND PROBLEM 
 
-// function budgetCalculator(watch, phone, laptop){
+function budgetCalculator(watch, phone, laptop){
     
-//     if (watch, phone, laptop != 0 && watch, phone, laptop >0){
-//         var watch = 50 * watch;
-//         var phone = 100 * phone;
-//         var laptop = 500 * laptop;
-//         var result = watch + phone + laptop;
-//     }
-//     // if any one enter single value
-//     else if(watch == 0){
-//         console.log("You can't enter any single or double value!!")
-//     }
-//     else{
-//         console.log("You must be input valid number for calculate your price!!!!!!!!")
-//     }
-//     return result;
-// }
+    if (watch, phone, laptop != 0 && watch, phone, laptop >0){
+        var watch = 50 * watch;
+        var phone = 100 * phone;
+        var laptop = 500 * laptop;
+        var result = watch + phone + laptop;
+    }
+    // if any one enter single value
+    else if(watch == 0){
+        console.log("You can't enter any single or double value!!")
+    }
+    else{
+        console.log("You must be input valid number for calculate your price!!!!!!!!")
+    }
+    return result;
+}
 
-// var budgetInput = budgetCalculator(3,2,3);
-// console.log(budgetInput)
-
-
-// ////// Third PROBLEM 
-
-// function hotelCost(rent){
-//     if(rent > 0){
-//         var hotelRent = 100;
-//         if (rent <= 10){
-//             var totalCost = rent * 100;
-//         }
-//         else if (rent < 10 || 21 > rent){
-//             var totalCost = rent * (100-20);
-//         }
-//         else if(20 < rent){
-//             var totalCost = rent * (100-50);
-//         }
-//         else{
-//             console.log("You can't stay here without money or you must be pay for stay!!!!!!");
-//         }
-//         return totalCost;
-//     }
-//     else{
-//         console.log("You must be stay here one day!!!!")
-//     }
-// }
-
-// var payMoneyForHotel = hotelCost(0);
-// console.log(payMoneyForHotel)
+var budgetInput = budgetCalculator();
+console.log(budgetInput)
 
 
-// ////// FOURTH PROBLEM 
+////// Third PROBLEM 
+
+function hotelCost(rent){
+    if(rent > 0){
+        var hotelRent = 100;
+        if (rent <= 10){
+            var totalCost = rent * 100;
+        }
+        else if (rent < 10 || 21 > rent){
+            var totalCost = rent * (100-20);
+        }
+        else if(20 < rent){
+            var totalCost = rent * (100-50);
+        }
+        else{
+            console.log("You can't stay here without money or you must be pay for stay!!!!!!");
+        }
+        return totalCost;
+    }
+    else{
+        console.log("You must be stay here one day!!!!")
+    }
+}
+
+var payMoneyForHotel = hotelCost(33);
+console.log(payMoneyForHotel)
+
+
+////// FOURTH PROBLEM 
 
 function megaFriend(friendList){
     if (friendList != ""){
         var bigName = "";
-    for(var i = 0; i < friendList.length; i++){
+        for(var i = 0; i < friendList.length; i++){
         if(bigName.length < friendList[i].length){
             bigName = friendList[i];
+            }
         }
-    }
-    return bigName;
+        return bigName;
     }
     else{
         console.log('Enter your friends name for find your magefriend')
