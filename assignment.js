e31309cdbfe22dc4b3cd3dc245fc2a35529a060f
mduumar112233 <mdumarfaruk187326@gmail.com>
2021-01-27 22:37:06 -0800

    adding

--- assignment.js
+++ assignment.js
@@ -1,91 +1,95 @@
-////// FIRST PROBLEM 
+// ////// FIRST PROBLEM 
 
-function kilometerToMeter(meter){
-    if (meter != 0 && meter > 0){
-        var result = meter * 1000;
-    }
-    else{
-        console.log("You must be input valid number for check this function!!!!!!!!")
-    }
-    return result;
-}
+// function kilometerToMeter(meter){
+//     if (meter != 0 && meter > 0){
+//         var result = meter * 1000;
+//     }
+//     else{
+//         console.log("You must be input valid number for check this function!!!!!!!!")
+//     }
+//     return result;
+// }
 
-var userInput = kilometerToMeter(3);
-console.log(userInput)
+// var userInput = kilometerToMeter(3);
+// console.log(userInput)
 
-////// SECOND PROBLEM 
+// ////// SECOND PROBLEM 
 
-function budgetCalculator(watch, phone, laptop){
+// function budgetCalculator(watch, phone, laptop){
     
-    if (watch, phone, laptop != 0 && watch, phone, laptop >0){
-        var watch = 50 * watch;
-        var phone = 100 * phone;
-        var laptop = 500 * laptop;
-        var result = watch + phone + laptop;
-    }
-    // if any one enter single value
-    else if(watch == 0){
-        console.log("You can't enter any single or double value!!")
-    }
-    else{
-        console.log("You must be input valid number for calculate your price!!!!!!!!")
-    }
-    return result;
-}
+//     if (watch, phone, laptop != 0 && watch, phone, laptop >0){
+//         var watch = 50 * watch;
+//         var phone = 100 * phone;
+//         var laptop = 500 * laptop;
+//         var result = watch + phone + laptop;
+//     }
+//     // if any one enter single value
+//     else if(watch == 0){
+//         console.log("You can't enter any single or double value!!")
+//     }
+//     else{
+//         console.log("You must be input valid number for calculate your price!!!!!!!!")
+//     }
+//     return result;
+// }
 
-var budgetInput = budgetCalculator();
-console.log(budgetInput)
+// var budgetInput = budgetCalculator();
+// console.log(budgetInput)
 
 
-////// Third PROBLEM 
+// ////// Third PROBLEM 
 
 function hotelCost(rent){
     if(rent > 0){
-        var hotelRent = 100;
         if (rent <= 10){
-            var totalCost = rent * 100;
+            var newTotalCost = rent * 100;
         }
         else if (rent < 10 || 21 > rent){
-            var totalCost = rent * (100-20);
+            var afterTenDays = rent - 10;
+            var totalCost = afterTenDays * 80;
+            var newTotalCost = totalCost + 1000;
+
         }
         else if(20 < rent){
-            var totalCost = rent * (100-50);
+            var aftertwentydays = rent - 20;
+            var totalCost = aftertwentydays * 50;
+            var newTotalCost = totalCost + 1800;
         }
         else{
             console.log("You can't stay here without money or you must be pay for stay!!!!!!");
         }
-        return totalCost;
+        return newTotalCost;
     }
     else{
         console.log("You must be stay here one day!!!!")
     }
 }
 
-var payMoneyForHotel = hotelCost(33);
+var payMoneyForHotel = hotelCost(22);
 console.log(payMoneyForHotel)
 
 
-////// FOURTH PROBLEM 
+// ////// FOURTH PROBLEM 
 
-function megaFriend(friendList){
-    if (friendList != ""){
-        var bigName = "";
-        for(var i = 0; i < friendList.length; i++){
-        if(bigName.length < friendList[i].length){
-            bigName = friendList[i];
-            }
-        }
-        return bigName;
-    }
-    else{
-        console.log('Enter your friends name for find your magefriend')
-    }
-}
+// function megaFriend(friendList){
+//     if (friendList != ""){
+//         var bigName = "";
+//         for(var i = 0; i < friendList.length; i++){
+//         if(bigName.length < friendList[i].length){
+//             bigName = friendList[i];
+//             }
+//         }
+//         return bigName;
+//     }
+//     else{
+//         console.log('Enter your friends name for find your magefriend')
+//     }
+// }
 
-var allFriend = ['sagor', 'rafi', 'mosadek', 'robul', 'sahajalal'];
+// var allFriend = ['sagor', 'rafi', 'mosadek', 'robul', 'sahajalal'];
 
-var result = megaFriend(allFriend);
-console.log(result);
+// var result = megaFriend(allFriend);
+// console.log(result);
 
 
 
